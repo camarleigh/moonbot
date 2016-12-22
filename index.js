e1d64bd835ae0119ad98f2dc6a38fbf3c5f5f125
marleigh <camarleigh@gmail.com>
2016-12-22 03:09:31 -0800

    wrote oauth function and error catch

--- index.js
+++ index.js
@@ -25,3 +25,34 @@ app.get('/', function(req,res){
   res.send('Ngrok-king it! You are hitting this path:' + req.url);
 });
 
+//GET request to SLACK /oauth endpoint
+app.get('oauth', function(req,res){
+  //when a user authorizes app, if code query is missing, throw error msg
+  if(!req.query.code){
+    res.status(500);
+    res.send({"Error":"Sad trombone. No code coming through."});
+    console.log("Sad trombone. No code coming through.");
+  } else {
+    //if code query there, proceed by passing id & secret & code
+    request({
+      //url
+      url: 'https://slack.com/api/oauth.access',
+      //query string data
+      qs:{code:req.query.code, slack_id: slackId, slack_secret: slackSecret},
+      //be sure to specify method
+      method: 'GET',
+    }, function(error, response, body){
+      if(error){
+        console.log(error);
+      }else{
+        res.json(body);
+      }
+    })
+
+  }
+
+  //endpoint that confirms ngrok tunnel is up and working
+  app.post('/command', function(req,res){
+    res.send('Yep, your ngrok is grokking!');
+  });
+});
